5e381a0153fb38a41e55e0b3c48901d465bf7573
Mike Landers <landers.mike@gmail.com>
2019-06-18 09:35:34 -0700

    minor fix

--- functions/index.js
+++ functions/index.js
@@ -37,7 +37,7 @@ app.post('/getGames', async (req, res) => {
             res.send(items);
         }
     } catch (error) {
-        res.status(500).json({message:'get games fail', data:error});
+        res.status(500).json({ message: 'get games fail', data: error });
     }
 });
 app.post('/addGame', async (req, res) => {
@@ -53,7 +53,7 @@ app.post('/addGame', async (req, res) => {
         const game = await gameRef.get();
         res.json({ message: 'add game success', data: game });
     } catch (error) {
-        res.status(500).json({message:'add game fail',data: error});
+        res.status(500).json({ message: 'add game fail', data: error });
     }
 });
 app.delete('/deleteGame/:id', async (req, res) => {
@@ -88,7 +88,7 @@ app.delete('/deleteGame/:id', async (req, res) => {
 
         res.status(204).json({ message: 'game delete success' });
     } catch (error) {
-        res.status(500).json({message:'game delete fail',data: error});
+        res.status(500).json({ message: 'game delete fail', data: error });
     }
 });
 app.post('/addQuestion', async (req, res) => {
@@ -104,21 +104,22 @@ app.post('/addQuestion', async (req, res) => {
         });
         res.status(200).json({ message: 'add question success' });
     } catch (error) {
-        res.status(500).json({message:'add question fail',data: error});
+        res.status(500).json({ message: 'add question fail', data: error });
     }
 });
 app.delete('/deleteQuestion/', (req, res) => {
-    try{
+    try {
         console.log('/deleteQuestion');
         const { id } = req.body;
-    
+
         db.collection('questions')
             .doc(id)
-            .delete()
+            .delete();
 
-            res.status(204).json({message:'question delete success'})
-    }catch(error){
-        res.status(500).json({message:'question delete fail', data: error});
+        res.status(204).json({ message: 'question delete success' });
+    } catch (error) {
+        res.status(500).json({ message: 'question delete fail', data: error });
+    }
 });
 
 exports.jeopardy = functions.https.onRequest(app);
