f09cd3cdcc70af6f5b2b6cdf465e4150ce2d604e
Michael Landers <landers.mike@gmail.com>
2019-06-26 21:33:18 -0700

    fixed issues with getting single game

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 // admin.initializeApp(functions.config().firebase);
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -14,112 +14,127 @@ app.use(cors({ origin: true }));
 // Add middleware to authenticate requests
 // app.use(myMiddleware);
 
-app.post('/getGames', async (req, res) => {
-    console.log('/getGames');
-    try {
-        let { uid } = req.body;
-        let items = [];
-        uid = uid.toString();
+app.post("/getGames", async (req, res) => {
+	console.log("/getGames");
+	try {
+		let { uid } = req.body;
+		let items = [];
+		uid = uid.toString();
 
-        const snapshot = await db
-            .collection('games')
-            .where('author', '==', uid)
-            .get();
+		const snapshot = await db
+			.collection("games")
+			.where("author", "==", uid)
+			.get();
 
-        snapshot.docs.forEach(doc => {
-            items.push(doc.data());
-        });
+		snapshot.docs.forEach(doc => {
+			items.push(doc.data());
+		});
 
-        if (snapshot.empty) {
-            res.send('No documents');
-        } else {
-            items = items.sort();
-            res.send(items);
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'get games fail', data: error });
-    }
+		if (snapshot.empty) {
+			res.status(200).json({ message: "No documents" });
+		} else {
+			items = items.sort();
+			res.send(items);
+		}
+	} catch (error) {
+		res.status(500).json({ message: "get games fail", data: error });
+	}
 });
-app.post('/addGame', async (req, res) => {
-    try {
-        console.log('/addGame');
-        const { name, uid } = req.body;
-        const data = {
-            gameName: name,
-            author: uid
-        };
+app.get("/getGame/:id", async (req, res) => {
+	console.log("/getGames");
+	try {
+		let { id } = req.params;
 
-        const gameRef = await db.collection('games').add(data);
-        const game = await gameRef.get();
-        res.json({ message: 'add game success', data: game });
-    } catch (error) {
-        res.status(500).json({ message: 'add game fail', data: error });
-    }
+		const game = await db
+			.collection("games")
+			.doc(id)
+			.get();
+
+		res.status(200).json({ message: "get game success", data: game.data() });
+	} catch (error) {
+		res.status(500).json({ message: "get game fail", data: error });
+	}
 });
-app.delete('/deleteGame/:id', async (req, res) => {
-    try {
-        console.log(`/deleteGame/${req.params.id}`);
-        const { id } = req.params;
+app.post("/addGame", async (req, res) => {
+	try {
+		console.log("/addGame");
+		const { name, uid } = req.body;
+		const data = {
+			gameName: name,
+			author: uid
+		};
 
-        const questions = await db
-            .collection('questions')
-            .where('gameID', '==', id)
-            .get();
-        let questionIDs = [];
-        questions.docs.forEach(doc => {
-            questionIDs.push(doc.id);
-        });
-        if (questionIDs.length > 0) {
-            questionIDs.forEach(id => {
-                db.collection('questions')
-                    .doc(id)
-                    .delete();
-            });
-            await db
-                .collection('games')
-                .doc(id)
-                .delete();
-        } else {
-            await db
-                .collection('games')
-                .doc(id)
-                .delete();
-        }
-
-        res.status(204).json({ message: 'game delete success' });
-    } catch (error) {
-        res.status(500).json({ message: 'game delete fail', data: error });
-    }
+		const gameRef = await db.collection("games").add(data);
+		const game = await gameRef.get();
+		res.json({ message: "add game success", data: game });
+	} catch (error) {
+		res.status(500).json({ message: "add game fail", data: error });
+	}
 });
-app.post('/addQuestion', async (req, res) => {
-    try {
-        console.log('/addQuestion');
-        const { question, answer, gameID, uid } = req.body;
+app.delete("/deleteGame/:id", async (req, res) => {
+	try {
+		console.log(`/deleteGame/${req.params.id}`);
+		const { id } = req.params;
 
-        db.collection('questions').add({
-            question: question,
-            answer: answer,
-            gameID: gameID,
-            author: uid
-        });
-        res.status(200).json({ message: 'add question success' });
-    } catch (error) {
-        res.status(500).json({ message: 'add question fail', data: error });
-    }
+		const questions = await db
+			.collection("questions")
+			.where("gameID", "==", id)
+			.get();
+		let questionIDs = [];
+		questions.docs.forEach(doc => {
+			questionIDs.push(doc.id);
+		});
+		if (questionIDs.length > 0) {
+			questionIDs.forEach(id => {
+				db.collection("questions")
+					.doc(id)
+					.delete();
+			});
+			await db
+				.collection("games")
+				.doc(id)
+				.delete();
+		} else {
+			await db
+				.collection("games")
+				.doc(id)
+				.delete();
+		}
+
+		res.status(204).json({ message: "game delete success" });
+	} catch (error) {
+		res.status(500).json({ message: "game delete fail", data: error });
+	}
 });
-app.delete('/deleteQuestion/', (req, res) => {
-    try {
-        console.log('/deleteQuestion');
-        const { id } = req.body;
+app.post("/addQuestion", async (req, res) => {
+	try {
+		console.log("/addQuestion");
+		const { question, answer, gameID, uid } = req.body;
 
-        db.collection('questions')
-            .doc(id)
-            .delete();
+		db.collection("questions").add({
+			question: question,
+			answer: answer,
+			gameID: gameID,
+			author: uid
+		});
+		res.status(200).json({ message: "add question success" });
+	} catch (error) {
+		res.status(500).json({ message: "add question fail", data: error });
+	}
+});
+app.delete("/deleteQuestion/", (req, res) => {
+	try {
+		console.log("/deleteQuestion");
+		const { id } = req.body;
 
-        res.status(204).json({ message: 'question delete success' });
-    } catch (error) {
-        res.status(500).json({ message: 'question delete fail', data: error });
-    }
+		db.collection("questions")
+			.doc(id)
+			.delete();
+
+		res.status(204).json({ message: "question delete success" });
+	} catch (error) {
+		res.status(500).json({ message: "question delete fail", data: error });
+	}
 });
 
 exports.jeopardy = functions.https.onRequest(app);
